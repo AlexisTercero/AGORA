caa66f4abe38150118c91606dc0c0655d86c25f8
alexistercero <alexisdealmeyda@gmail.com>
2022-11-04 19:27:03 -0300

    handling selections on ListItem

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -40,7 +40,7 @@ function MessagesScreen(props) {
             title={item.title}
             subTitle={item.description}
             image={item.image}
-            // onPress={() => console.log('Message selected', item)}
+            onPress={() => console.log('Item pressed', item)}
             // renderRightActions={() => (
             //   <ListItemDeleteAction onPress={() => handleDelete(item)} />
             // )}

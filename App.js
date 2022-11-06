146356295c1a89145844df67b03594d6246cbd38
alexistercero <alexisdealmeyda@gmail.com>
2022-11-06 12:59:20 -0300

    Add PickerItem and Modal

--- App.js
+++ App.js
@@ -9,10 +9,16 @@ import ListingScreen from './app/screens/ListingScreen';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Cameras', value: 3 },
+];
+
 export default function App() {
   return (
     <Screen>
-      <AppPicker placeholder="Category" icon="apps" />
+      <AppPicker items={categories} placeholder="Category" icon="apps" />
       <AppTextInput placeholder="Email" icon="email" />
     </Screen>
   );

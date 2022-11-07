55db3643c201ecd9bc15906c1aaa117bde0b6a7e
alexistercero <alexisdealmeyda@gmail.com>
2022-11-07 11:28:36 -0300

    Add Touched state

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -22,30 +22,32 @@ function LoginScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors }) => (
+        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
             <AppTextInput
               autoCapitalize="none" //evita que automaticamente la primer letra del email se vuelva mayuscula
               autoCorrect={false} //evita correccion ortografica automatica
               icon="email"
               keyboardType="email-address" //facilita el tipeo de una direccion de email
+              onBlur={() => setFieldTouched('email')}
               onChangeText={handleChange('email')}
               placeholder="Email"
               textContentType="emailAddress" //ios permite autocompletar desde la keychain
             />
-            <ErrorMessage error={errors.email} />
+            <ErrorMessage error={errors.email} visible={touched.email} />
 
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
+              onBlur={() => setFieldTouched('password')}
               onChangeText={handleChange('password')}
               placeholder="Password"
               secureTextEntry={true}
               textContentType="password"
             />
 
-            <ErrorMessage error={errors.password} />
+            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}

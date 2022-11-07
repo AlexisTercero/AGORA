7f5486b95642a85e8a6ebe3538afa622ea12c2da
alexistercero <alexisdealmeyda@gmail.com>
2022-11-07 11:56:56 -0300

    Add SubmitButton component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -6,6 +6,7 @@ import * as Yup from 'yup';
 import AppButton from '../components/AppButton';
 import Screen from '../components/Screen';
 import AppFormField from '../components/AppFormField';
+import SubmitButton from '../components/SubmitButton';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
@@ -21,7 +22,7 @@ function LoginScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+        {() => (
           <>
             <AppFormField
               autoCapitalize="none" //evita que automaticamente la primer letra del email se vuelva mayuscula
@@ -42,7 +43,7 @@ function LoginScreen(props) {
               secureTextEntry={true}
               textContentType="password"
             />
-            <AppButton title="Login" onPress={handleSubmit} />
+            <SubmitButton title="Login" />
           </>
         )}
       </Formik>

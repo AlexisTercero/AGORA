26f338bcf6828f5a4a13e354c5f88653f852240d
alexistercero <alexisdealmeyda@gmail.com>
2022-11-07 18:45:00 -0300

    Screen fix

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -6,7 +6,7 @@ import { SafeAreaView, StyleSheet, View } from 'react-native';
 function Screen({ children, style }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View style={style}>{children}</View>
+      <View style={[styles.view, style]}>{children}</View>
     </SafeAreaView>
   );
 }
@@ -15,6 +15,9 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     paddingTop: Constants.statusBarHeight, //obtengo la altura exacta de la status bar del dispositivo por medio de Constants.
+  },
+  view: {
+    flex: 1,
   },
 });
 

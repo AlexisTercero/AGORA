28204f74e77535b81c5c7508fab7f101274ce6cb
alexistercero <alexisdealmeyda@gmail.com>
2022-11-07 10:49:41 -0300

    Add Formik

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Image, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
 import Screen from '../components/Screen';
 
 function LoginScreen(props) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-      <AppTextInput
-        autoCapitalize="none" //evita que automaticamente la primer letra del email se vuelva mayuscula
-        autoCorrect={false} //evita correccion ortografica automatica
-        icon="email"
-        keyboardType="email-address" //facilita el tipeo de una direccion de email
-        onChangeText={(text) => setEmail(text)}
-        placeholder="Email"
-        textContentType="emailAddress" //ios permite autocompletar desde la keychain
-      />
-      <AppTextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="lock"
-        onChangeText={(text) => setPassword(text)}
-        placeholder="Password"
-        secureTextEntry={true}
-        textContentType="password"
-      />
-      <AppButton title="Login" onPress={() => console.log(email, password)} />
+      <Formik
+        initialValues={{ email: '', password: ' ' }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none" //evita que automaticamente la primer letra del email se vuelva mayuscula
+              autoCorrect={false} //evita correccion ortografica automatica
+              icon="email"
+              keyboardType="email-address" //facilita el tipeo de una direccion de email
+              onChangeText={handleChange('email')}
+              placeholder="Email"
+              textContentType="emailAddress" //ios permite autocompletar desde la keychain
+            />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onChangeText={handleChange('password')}
+              placeholder="Password"
+              secureTextEntry={true}
+              textContentType="password"
+            />
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 }

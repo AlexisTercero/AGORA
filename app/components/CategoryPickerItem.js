735113f849018d44bd83939febf4b1165e8d4e62
alexistercero <alexisdealmeyda@gmail.com>
2022-11-07 23:08:27 -0300

    Add CategoryPickerItem

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from './Icon';
+import AppText from './AppText';
 
-function CategoryPickerItem(props) {
-  return null;
+function CategoryPickerItem({ item, onPress }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={70}
+        />
+      </TouchableOpacity>
+      <AppText style={styles.label}>{item.label}</AppText>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 18,
+    paddingVertical: 15,
+    alignItems: 'center',
+    width: '33%',
+  },
+  label: {
+    fontSize: 15,
+    marginTop: 5,
+    textAlign: 'center',
+  },
+});
 
 export default CategoryPickerItem;

879b427ee64be1921c239f3cb0b0724ec8d710a5
alexistercero <alexisdealmeyda@gmail.com>
2022-11-09 18:26:32 -0300

    Add api conection

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
 import Card from '../components/Card';
 import colors from '../config/colors';
+import listingsApi from '../api/listings';
 import routes from '../navigation/routes';
 import Screen from '../components/Screen';
 
-const listings = [
-  {
-    id: 1,
-    title: 'Leather jacket for sale',
-    price: 300,
-    image: require('../assets/leather-jacket.jpeg'),
-  },
-  {
-    id: 2,
-    title: 'Pink Couch',
-    price: 3000,
-    image: require('../assets/pink-couch.jpeg'),
-  },
-];
+// const listings = [
+//   {
+//     id: 1,
+//     title: 'Leather jacket for sale',
+//     price: 300,
+//     image: require('../assets/leather-jacket.jpeg'),
+//   },
+//   {
+//     id: 2,
+//     title: 'Pink Couch',
+//     price: 3000,
+//     image: require('../assets/pink-couch.jpeg'),
+//   },
+// ];
 
 function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -31,7 +43,7 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={'$' + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}

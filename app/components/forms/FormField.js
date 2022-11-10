0c9dff8c1f858353c495fbbfb6fac755aea90a39
alexistercero <alexisdealmeyda@gmail.com>
2022-11-10 12:19:16 -0300

    Add loading animations an form reset

--- app/components/forms/FormField.js
+++ app/components/forms/FormField.js
@@ -5,13 +5,15 @@ import TextInput from '../TextInput';
 import ErrorMessage from './ErrorMessage';
 
 function AppFormField({ name, ...otherProps }) {
-  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+  const { setFieldTouched, setFieldValue, errors, touched, values } =
+    useFormikContext();
 
   return (
     <>
       <TextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={(text) => setFieldValue(name, text)}
+        value={values[name]}
         {...otherProps}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
+import {
+  Form,
+  FormField,
+  FormPicker as Picker,
+  SubmitButton,
+} from '../components/forms';
 import CategoryPickerItem from '../components/CategoryPickerItem';
-import { Form, FormField, FormPicker, SubmitButton } from '../components/forms';
+import Screen from '../components/Screen';
 import FormImagePicker from '../components/forms/FormImagePicker';
 import listingsApi from '../api/listings';
-import Screen from '../components/Screen';
 import useLocation from '../hooks/useLocation';
+import UploadScreen from './UploadScreen';
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label('Title'),
   price: Yup.number().required().min(1).max(10000).label('Price'),
   description: Yup.string().label('Description'),
   category: Yup.object().required().nullable().label('Category'),
-  images: Yup.array().min(1, 'Please select at least one image'),
+  images: Yup.array().min(1, 'Please select at least one image.'),
 });
 
 const categories = [
@@ -76,19 +82,32 @@ const categories = [
 
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, { resetForm }) => {
+    setProgress(0);
+    setUploadVisible(true);
     const result = await listingsApi.addListing(
       { ...listing, location },
-      (progress) => console.log(progress)
+      (progress) => setProgress(progress)
     );
-    if (!result.ok) return alert('Could not save the listing.');
 
-    alert('Success');
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert('Could not save the listing');
+    }
+
+    resetForm();
   };
 
   return (
     <Screen style={styles.container}>
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
       <Form
         initialValues={{
           title: '',
@@ -107,13 +126,15 @@ function ListingEditScreen() {
           maxLength={8}
           name="price"
           placeholder="Price"
+          width={120}
         />
-        <FormPicker
+        <Picker
           items={categories}
           name="category"
           numberOfColumns={3}
           PickerItemComponent={CategoryPickerItem}
           placeholder="Category"
+          width="50%"
         />
         <FormField
           maxLength={255}

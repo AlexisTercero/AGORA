1e86570f8620d8b0fcf579712b59a996eb095e79
alexistercero <alexisdealmeyda@gmail.com>
2022-11-11 11:26:43 -0300

    Add AppLoading

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import jwtDecode from 'jwt-decode';
+import AppLoading from 'expo-app-loading';
 
 import authStorage from './app/auth/storage';
 import navigationTheme from './app/navigation/navigationTheme';
@@ -10,16 +11,22 @@ import AuthContext from './app/auth/context';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
     if (!token) return;
     setUser(jwtDecode(token));
   };
-  useEffect(() => {
-    restoreToken();
-  }, []);
 
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <NavigationContainer theme={navigationTheme}>

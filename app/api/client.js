a57fb3b0bbe06783fd2f58266f756465fdd4db76
alexistercero <alexisdealmeyda@gmail.com>
2022-11-11 15:15:05 -0300

    Add useAuth custom hook

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,14 @@
 import { create } from 'apisauce';
+import autoStorage from '../auth/storage';
 
 const apiClient = create({
   baseURL: 'http://192.168.0.127:9000/api/',
+});
+
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = autoStorage.getToken();
+  if (!authToken) return;
+  request.headers['x-auth-token'] = authToken;
 });
 
 export default apiClient;

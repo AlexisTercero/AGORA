052d8db93e618282b2c5d346298f657f612ecb87
alexistercero <alexisdealmeyda@gmail.com>
2022-11-11 10:46:31 -0300

    Add storage to keep user logged in

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import jwtDecode from 'jwt-decode';
 
+import authStorage from './app/auth/storage';
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import AuthNavigator from './app/navigation/AuthNavigation';
@@ -8,6 +10,15 @@ import AuthContext from './app/auth/context';
 
 export default function App() {
   const [user, setUser] = useState();
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken();
+    if (!token) return;
+    setUser(jwtDecode(token));
+  };
+  useEffect(() => {
+    restoreToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
